49727d921e758bd672bf2a4b58f5a8de20b3e1cb
akhil dekarla <akhil.dekarla@hexagon.com>
2025-08-04 23:56:29 +0530

    first_draft

--- lib/src/print_fireflies.ex
+++
@@ -1,5 +0,0 @@
-defmodule FireFly.Source do
-  def printFireFlies do
-    IO.puts("Hi")
-  end
-end
--- lib/src/task.ex
+++
@@ -1,5 +0,0 @@
-defmodule FireFly.Source.Task do
-  def task do
-    :timer.apply_interval(100, IO, :puts, ["hello"])
-  end
-end
